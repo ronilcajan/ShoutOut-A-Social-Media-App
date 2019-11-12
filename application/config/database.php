3f17b7ffab6a498bc31c3f2f2efe27e18dec9ecd
Ronil Cajan <cajanr02@gmail.com>
2019-11-12 20:22:41 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'shoutout-mysqldbserver.mysql.database.azure.com',
 	'username' => 'ron@shoutout-mysqldbserver',
 	'password' => 'kagebunshin1!',
 	'database' => 'shoutout',
