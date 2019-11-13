0e91bf707599d2ae9b0eb9c73aa4b4c0c4140bd0
ronilcajan <bugsgamer00@gmail.com>
2019-11-13 22:22:39 +0800

    Add responsive design to home page and about page

--- application/views/about.php
+++ application/views/about.php
@@ -6,12 +6,12 @@
             <a class="navs" href="<? echo base_url();?>about">About</a>
         </div>
         <nav>
-            <a href="<? echo base_url();?>signup" class="nav-link">Sign up</a>
-            <a href="<? echo base_url();?>login" class="nav-link">Log in</a>
+            <a href="<? echo base_url();?>signup" class="nav-link">Signup</a>
+            <a href="<? echo base_url();?>login" class="nav-link">Login</a>
         </nav>
     </header>
     <main>
-        <h3 class="shout">ShoutOut is</h3><br>
+        <h3 class="shout font-weight-bold">ShoutOut is</h3><br>
         <h1 class="main-text">what’s happening in the world and what people are talking about right now.</h1>
     </main>
 
--- application/views/templates/header-home.php
+++ application/views/templates/header-home.php
@@ -4,4 +4,6 @@
     <? $this->load->view('templates/main-header') ?>
     <title>ShoutOut. Show what you think.</title>
     <link type="text/css" href="<?php echo base_url(); ?>bootstrap/css/mystyle.css" rel="stylesheet">
+    <link type="text/css" href="<?php echo base_url(); ?>bootstrap/css/media_query.css" rel="stylesheet">
+
 </head>
