e3cd724c362ae49958878befe6203275ff1c54ca
ronilcajan <bugsgamer00@gmail.com>
2019-11-14 09:25:53 +0800

    Added login responsive design

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,7 @@
                 <a class="navs" href="<? echo base_url();?>about">About</a>
                 </div>
             <nav>
-                <a href="<? echo base_url();?>signup" class="nav-link">Sign in</a>
+                <a href="<? echo base_url();?>signup" class="nav-link">Signup</a>
             </nav>
         </div>
     </header>
@@ -24,8 +24,8 @@
         <div class="login-form-container">
             <h3 class="login-text">Log in to ShoutOut</h3>
             <form class="login-form" action="<? echo base_url();?>login-user" method="POST">
-                <input type="text" class="form-control form-control-sm username" name="username" placeholder="Username or email"/>
-                <input type="password" class="form-control form-control-sm password" name="password" placeholder="Password"/>
+                <input type="text" class="form-control form-control-sm username" name="username" placeholder="Username" required/>
+                <input type="password" class="form-control form-control-sm password" name="password" placeholder="Password" required/>
                 <div class="btn-container">
                     <button type="submit" class="login-btn">Log in</button>
                 </div>
