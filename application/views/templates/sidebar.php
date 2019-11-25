be3ff197106500d84e8bd42622750d74faeac98d
ronilcajan <bugsgamer00@gmail.com>
2019-11-25 23:10:15 +0800

    add search forms

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,7 +1,7 @@
 <div class="row position-fixed">
     <div class="col-md-11 m-1">
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+        <form class="form-inline my-2 my-lg-0" action="<? echo base_url();?>search" method="POST">
+            <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search name..">
             <button class="btn btn-primary my-2 my-sm-0 rounded-pill " type="submit">Search</button>
         </form>
     </div>
