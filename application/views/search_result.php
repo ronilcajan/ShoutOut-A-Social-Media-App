95991351abf45e055ce04a825e75fc036a509d57
ronilcajan <bugsgamer00@gmail.com>
2019-11-26 17:05:38 +0800

    Add search page

--- application/views/search_result.php
+++ application/views/search_result.php
@@ -4,45 +4,78 @@
         <div class="col-3 pl-5 border-right">
             <? $this->load->view('templates/navs');?>
         </div>
-        <div class="col-6 pl-0 pr-0" style="background-color: #FFCCCB;height:100vh;">
+        <div class="col-6 pl-0 pr-0" style="background-color: #FFCCCB;">
             <div class="bg-secondary position-fixed home-top w-100 border-bottom" style="height:50px; z-index:1;">
-                <h4 class="mt-2 ml-2 font-weight-bold">Search Results</h4>
+                <h4 class="mt-2 ml-2 font-weight-bold text-primary">Search Results</h4>
             </div>
             <div class="col mt-5 pt-1 pl-1 pr-1">
                 <div class="card bg-secondary mt-3" >
                     <div class="card-header font-weight-bold text-primary">People
                     </div>
                 </div>
+                <? if(!empty($people)){?>
+                <? foreach($people as $k => $person){?>
                 <div class="card bg-secondary mt-3" >
                     <div class="card-body font-weight-bold form-inline">
-                        <img class="rounded-circle border mr-2" src="<? echo base_url();?>uploads/default.png" width="50" height="50"/>
-                        <p class="mt-3">Ronil Cajan</p>
+                        <img class="rounded-circle border mr-2" src="<? echo base_url().'uploads/'.$person['image'];?>" width="50" height="50"/>
+                        <a href="<? echo base_url().'username/'.$person['name'];?>">
+                            <p class="mt-3"><? echo $person['name'];?></p>
+                        </a>
                     </div>
                 </div>
-                <div class="card bg-secondary mt-3" >
-                    <div class="card-body font-weight-bold form-inline">
-                        <img class="rounded-circle border mr-2" src="<? echo base_url();?>uploads/default.png" width="50" height="50"/>
-                        <p class="mt-3">Ronil Cajan</p>
-                    </div>
-                </div>
-                <div class="card bg-secondary mt-3" >
-                    <div class="card-body font-weight-bold form-inline">
-                        <img class="rounded-circle border mr-2" src="<? echo base_url();?>uploads/default.png" width="50" height="50"/>
-                        <p class="mt-3">Ronil Cajan</p>
-                    </div>
-                </div>
-
+                <?}?>
+                <?}else{?>
+                    <h3 class="font-italic text-center mt-3 text-muted">----Not found-----</h3>
+                <?}?>
                 <div class="card bg-secondary mt-5" >
                     <div class="card-header font-weight-bold text-primary">Post
                     </div>
                 </div>
+                <? if(!empty($post)){?> 
+                <? foreach($post as $k => $posts){?>
+                <div class="card bg-secondary mt-3" >
+                    <div class="card-header" style="height:75px">
+                        <img class="rounded-circle border mr-2" src="<? echo base_url().'uploads/'.$posts['image']; ?>" width="50" height="50"/>
+                        <a href="<? echo base_url().'username/'.$posts['user'];?>">
+                            <span class=" font-weight-bold"><? echo $posts['name']; ?></span>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"><? echo $posts['post'];?></p>
+                        <? if(!empty($posts['post_image'])){?>
+                            <div class="w-100 text-center">
+                                <img class="img img-fluid border p-1" src="<? echo base_url().'uploads/'.$posts['post_image']; ?>"/>
+                            </div>
+                        <? }?>
+                        <div class="row w-100 bg-danger mt-3 ml-1">
+                            <div class="col-md-6 pl-0 pr-0">
+                                <form method="post" action="<? echo base_url().'claps/'.$posts['id'];?>">                        
+                                    <? if($posts['claps'] == 0){?>
+                                        <button class="col-md-12 border-0 bg-secondary claps" type="submit">Claps</button>
+                                    <? }else{ ?>
+                                        <button class="col-md-12 border-0 bg-secondary claps text-primary" type="submit">Claps(<? echo $posts['claps'];?>)</button>                                   
+                                    <?}?>       
+                                </form>
+                            </div>
+                            <? if($posts['comments'] == 0){?>
+                            <button class="col-md-6 border-0 bg-secondary">Comments</button>
+                            <?}else{?>
+                            <button class="col-md-6 border-0 bg-secondary text-primary">Comments(<? echo $posts['comments'];?>)</button>
+                                <?}?>
+                        </div>
+                    </div>
+                </div>
+                <?}?>
+                <?}else{?>
+                    <h3 class="font-italic text-center mt-3 text-muted">----Not found-----</h3>
+                <?}?>
             </div>
             
             <div class="col pt-3 pl-1 pr-1">
 
-                <!-- <nav aria-label="Page navigation example">
+                <nav aria-label="Page navigation example">
                     <p><? echo $links;?></p>
-                </nav> -->
+                </nav>
             </div>
         </div>
         <div class="col-3 bg-light border-left" style="z-index:2;">
