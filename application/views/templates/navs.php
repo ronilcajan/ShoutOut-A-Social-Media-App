7c7a2c9b9864190c736a651910f38206b3104dc2
ronilcajan <bugsgamer00@gmail.com>
2019-11-28 22:43:40 +0800

    Add change password dropdown

--- application/views/templates/navs.php
+++ application/views/templates/navs.php
@@ -14,10 +14,14 @@
             Profile
         </a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">
+    <li class="nav-item dropdown">
+ 
+        <a class="nav-link dropdown-toggle" id="dropdownMenuLink" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <img class="rounded-circle border ml-1 mr-2 border-primary" src="<? echo base_url();?>images/settings.png" class="m-2" width="40" height="40">
         Settings</a>
+         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="#change-password" data-target="#change-password" data-toggle="modal">Change Password</a>
+        </div>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="<? echo base_url();?>logout">
