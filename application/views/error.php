97125441a7a122c0a9d5f0c102723613f106950a
ronilcajan <bugsgamer00@gmail.com>
2019-12-02 17:59:07 +0800

    Improve Error page

--- application/views/error.php
+++ application/views/error.php
@@ -1,6 +1,8 @@
 <body class="bg-white">
     <center>
-    <div class="h-100 w-100 mt-5">
-    <img class="img-fluid" src="<? echo base_url();?>images/error2.jpg"/>
+    <div class="h-100 w-50 mt-5">
+    	<h1 class="text-light font-weight-bold">OOPS! ERROR 404!</h1>
+    	<p class="text-light">You have some trouble. The Web page you're looking for does not exist. Make sure you've typed the page correctly. It may have moved or try another link.</p>
+    	<button class="btn btn-outline-secondary font-weight-bold" onclick="location.href='<? echo base_url();?>'">Go home</button>
     </div>
     </center>
